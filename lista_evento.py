f9a011d4913417b04952bedf4baf3231dedca8ee
tteltola <139824016+tteltola@users.noreply.github.com>
2023-07-26 20:07:24 -0300

    Add files via upload

---
+++ lista_evento.py
@@ -0,0 +1,30 @@
+# lista de eventos
+
+[
+    {
+        "evento": "Salta Rok",
+        "imagen": "image_1.png",
+        "id_ubicacion": 1
+    },
+    {
+        "evento": "Lollapalooza",
+        "imagen": "image_2.png",
+        "id_ubicacion": 2
+    },
+    {
+        "evento": "Serenata a Cafayate",
+        "imagen": "image_3.png",
+        "id_ubicacion": 3
+    },
+    {
+        "evento": "Movistar Arena",
+        "imagen": "image_4.png",
+        "id_ubicacion": 4
+    },
+    {
+        "evento": "Cosquín Rock",
+        "imagen": "image_5.png",
+        "id_ubicacion": 5
+    }
+]
+
