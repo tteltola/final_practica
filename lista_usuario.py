0de8b58328248394196437349dc26be5fb4b4fa4
tteltola <139824016+tteltola@users.noreply.github.com>
2023-07-26 20:08:29 -0300

    Add files via upload

---
+++ lista_usuario.py
@@ -0,0 +1,25 @@
+# lista de usuario 
+
+[
+    {
+        "Usuario": "Liliana",
+        "id_ubicacion": 1
+    },
+    {
+        "Usuario": "Juan",
+        "id_ubicacion": 2
+    },
+    {
+        "Usuario": "Karina",
+        "id_ubicacion": 3
+    },
+    {
+        "Usuario": "Kaoru",
+        "id_ubicacion": 4
+    },
+    {
+        "Usuario": "Flor",
+        "id_ubicacion": 5
+    }
+]
+
